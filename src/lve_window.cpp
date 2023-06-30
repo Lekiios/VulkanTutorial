403d0422cdbe99fc17a462a1b30bfd57c0248a06
Lekiios <lorenzo.lacz@gmail.com>
2023-06-30 19:43:18 +0200

    - add engine device

--- src/lve_window.cpp
+++ src/lve_window.cpp
@@ -1,4 +1,5 @@
 #include "lve_window.hpp"
+#include <stdexcept>
 
 namespace lve {
 
@@ -19,6 +20,14 @@ void LveWindow::initWindow() {
 
   window =
       glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+}
+
+void LveWindow::createWindowSurface(VkInstance instance,
+                                    VkSurfaceKHR *surface) {
+  if (glfwCreateWindowSurface(instance, window, nullptr, surface) !=
+      VK_SUCCESS) {
+    throw std::runtime_error("Failed to create window surface !");
+  }
 }
 
 } // namespace lve
