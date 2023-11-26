18e7112116f3638d6cb1cdd41fb9f787c8bfcb42
Lekiios <lorenzo.lacz@gmail.com>
2023-11-26 13:05:04 +0100

    5 Tutorial done

--- src/first_app.hpp
+++ src/first_app.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_device.hpp"
 #include "lve_pipeline.hpp"
+#include "lve_swap_chain.hpp"
+
+#include <memory>
+#include <vector>
 
 namespace lve {
     class FirstApp {
@@ -9,15 +14,28 @@ namespace lve {
         static constexpr int WIDTH = 800;
         static constexpr int HEIGHT = 600;
 
+        FirstApp();
+        ~FirstApp();
+
+        FirstApp(const FirstApp&) = delete;
+        FirstApp &operator=(const FirstApp&) = delete;
+
         void run();
 
     private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan !"};
         LveDevice lveDevice{lveWindow};
-        LvePipeline lve_pipeline{
-                lveDevice,
-                "shaders/simple_shader.vert.spv",
-                "shaders/simple_shader.frag.spv",
-                LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+        LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+        std::unique_ptr<LvePipeline> lvePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
+        
+
+
     };
 }
